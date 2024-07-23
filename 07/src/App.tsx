9a9199245c4ae0dbd09cf70f9934b30df3f282f3
Kyoungeun Jo <kavvy99@naver.com>
2024-07-23 11:19:20 +0900

    day7: font setting

---
+++ 07/src/App.tsx
@@ -0,0 +1,5 @@
+import Fonts from "./components/learn/Fonts";
+
+export default function App() {
+  return <Fonts />;
+}
---
+++ 07/src/components/learn/Fonts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Fonts = () => {
+  return (
+    <>
+      <h1 className="inter text-3xl">Fonts Components</h1>
+      <h1 className="playwrite-be-vlg text-3xl">playwrite-be-vlg</h1>
+      <h1 className="malang text-3xl">한컴 말랑말랑</h1>
+      <h1 className="jeju text-3xl">제주돌담체</h1>
+    </>
+  );
+};
+
+export default Fonts;
