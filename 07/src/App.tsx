b64809f7758a095175e312b5df9a8f463c0e7094
Kyoungeun Jo <kavvy99@naver.com>
2024-07-23 20:29:39 +0900

    day7: create input, button component

--- 07/src/App.tsx
+++ 07/src/App.tsx
@@ -1,19 +1,24 @@
-import React from "react";
-
-import { twMerge } from "tailwind-merge";
-
-import RedColor from "./components/learn/children/RedColor";
+import Input from "./components/html/input";
+import Button from "./components/html/Button";
 
 const App = () => {
-  const isLoggedIn = true;
   return (
     <>
-      <div>App</div>
-      <RedColor>
-        I <span style={{ textDecoration: "underline" }}>want</span> this colored text
-      </RedColor>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="flex flex-col gap-2">
+          <Input
+            type="password"
+            placeholder=" Todo List!!!!!"
+            onChange={() => {
+              console.log("onchange");
+            }}
+          />
+          <Button className="bg-[#4F4F4F]">Add</Button>
+          <Button className="bg-[#ED4848]">Cancel</Button>
+          <Button className="bg-[#7D48ED]">Success</Button>
+        </div>
+      </div>
     </>
   );
 };
-
 export default App;
