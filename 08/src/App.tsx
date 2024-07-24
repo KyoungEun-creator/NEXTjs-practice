f450d1aaee7fa509be183e0212443a6b7cc20cb2
Kyoungeun Jo <kavvy99@naver.com>
2024-07-24 10:53:48 +0900

    day8: create SignIn component

--- 08/src/App.tsx
+++ 08/src/App.tsx
@@ -1,19 +1,7 @@
-import Button from "./components/html/Button";
-import Checkbox from "./components/html/Checkbox";
-import Input from "./components/html/Input";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="item-middle">
-      <div className="flex flex-col gap-4">
-        <Input type="text" placeholder="Enter your name" />
-        <Button className="bg-[#4b4b4b]">Add</Button>
-        <Button className="bg-[#ED4848]">Cancel</Button>
-        <Button className="bg-[#7D48ED]">Success</Button>
-        <Checkbox checked={true}>
-          I agree with <span className="font-bold">terms and policies.</span>
-        </Checkbox>
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
