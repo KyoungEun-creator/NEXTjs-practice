42992da409fb5d13b6b2421803963a546d4d2cac
Kyoungeun Jo <kavvy99@naver.com>
2024-07-26 00:03:39 +0900

    day9: split files from Todo using props

---
+++ 09/src/components/todo/TodoList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+import { TTodo } from "./Todo";
+
+const TodoList = ({
+  todos,
+  toggleTodo,
+  deleteTodo,
+}: {
+  todos: TTodo[];
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+}) => {
+  return (
+    <ul className="flex flex-col gap-4 mt-4 max-h-[284px] overflow-scroll">
+      {todos.map((todo) => {
+        return (
+          <TodoListItem key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default TodoList;
