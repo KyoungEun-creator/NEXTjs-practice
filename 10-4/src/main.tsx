02ff878c43990d8f475c9caf2951e08e55d242f2
Kyoungeun Jo <kavvy99@naver.com>
2024-07-30 08:50:44 +0900

    day10: react-router with createBrowserRouter

--- 10-4/src/main.tsx
+++ 10-4/src/main.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./assets/css/index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Write from "./pages/Write.tsx";
+import Detail from "./pages/Detail.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/detail/:id",
+    element: <Detail />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
